dcd4938df925a105c36aa78cecc637a1fc85e368
Ashish Singh <ashish@pop-os.localdomain>
2021-06-18 14:38:58 +0530

    Theme changer implemented

---
+++ script.js
@@ -0,0 +1,24 @@
+var themeButtons=document.getElementsByClassName('theme-mode')
+for(var i=0;i<themeButtons.length;i++)
+{
+    themeButtons[i].addEventListener('click',function(){
+        console.log("Clicked")
+        var theme=this.dataset.mode;
+        console.log(theme)
+        setTheme(theme)
+    })
+}
+function setTheme(mode){
+    if(mode==="dark")
+    {
+        document.getElementById('theme-style').href="dark.css";
+        document.getElementById('moon').style.display="none"
+        document.getElementById("sun").style.display = "inline";
+    }
+    if(mode==="light")
+    {
+        document.getElementById("theme-style").href = "";
+        document.getElementById("sun").style.display = "none";
+        document.getElementById("moon").style.display = "inline";
+    }
+}
